05f0efa0249a038e24a275dd98763a59cbe62e2f
paulbellmann <paul.bellmann@yahoo.de>
2017-10-05 01:43:30 +0200

    = woops

--- avocado/settings.py
+++ avocado/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'adasd834u24hjkfa8u32u_ads9312'
+SECRET_KEY = 'EDIT_ME'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
